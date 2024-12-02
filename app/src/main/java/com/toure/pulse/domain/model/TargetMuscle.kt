08250d7bf1caf5d3c3a34d6ad0716b9954a441ac
mehedit <mehedi.toure@gmail.com>
2024-12-02 16:48:54 +0100

    remove none required file for this branch

--- app/src/main/java/com/toure/pulse/domain/model/TargetMuscle.kt
+++
@@ -1,9 +0,0 @@
-package com.toure.pulse.domain.model
-
-data class TargetMuscle(
-    val bodyPartId: String,
-    val name: String,
-    val description: String,
-    val targetMuscleGroup: String, // e.g., Chest, Legs, Back
-    val muscleImageId: Int,
-)
--- app/src/main/java/com/toure/pulse/domain/model/WorkoutPlan.kt
+++
@@ -1,9 +0,0 @@
-package com.toure.pulse.domain.model
-
-data class WorkoutPlan(
-    val planId: String,
-    val name: String,
-    val description: String,
-    val goal: String, // e.g., Weight Loss, Muscle Gain, Endurance
-    val workouts: List<Workout>
-)
