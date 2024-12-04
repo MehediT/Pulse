76e7db619e584c13e639af693f177717fa478581
mehedit <mehedi.toure@gmail.com>
2024-12-04 11:45:42 +0100

    Add  - Init home content

--- app/src/main/java/com/toure/pulse/ui/PulseHomeContent.kt
+++ app/src/main/java/com/toure/pulse/ui/PulseHomeContent.kt
@@ -1,28 +1,15 @@
 package com.toure.pulse.ui
 
-
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import org.jetbrains.annotations.TestOnly
 
 @Composable
 fun PulseHomeContent(
     modifier: Modifier = Modifier,
-     innerPadding: PaddingValues = PaddingValues()) {
-    Column(
-        modifier = modifier.padding(innerPadding),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-    ) {
-    }
+) {
+
 }
 
-@Preview
-@Composable
-private fun PulseHomeContentPreview() {
-    PulseHomeContent()
-}
+
